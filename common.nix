e265819d493f4d664f1d2c7a8f518c6cb95676c4
Eric Ren <546296+rencire@users.noreply.github.com>
2020-03-29 17:08:34 -0700

    refactor: readded nur

--- common.nix
+++ common.nix
@@ -1,39 +1,32 @@
 with import ./nix {};
-[
-  # NUR
-  # https://github.com/nix-community/NUR 
-  nur.repos.rencire.neovim
+let 
+  myneovim = nur.repos.rencire.neovim.override {
+    configure = {
+      customRC = ''
+        "General vimrc
+        set number 
+      '';
+      packages.main = with vimPlugins; {
+        start = [
+          {
+            plugin = surround;
+            vimrc = ''
+              "Insert vimrc for surround plugin here
+            '';
+          }
+        ];
+      };
+    };
+  };
+in 
+  [
+    ## Default packages
+    # package management
+    niv
+    
+    # `nix` is removed when we run `nix-env -i ... --remove-all`
+    # Hence, we need to install it again.
+    nix 
 
-  ## Default packages
-  # package management
-  niv
-  
-  # `nix` is removed when we run `nix-env -i ... --remove-all`
-  # Hence, we need to install it again.
-  nix 
-
-  # Data processing
-  xsv     # replaaces column, cut
-
-  # VCS
-  git
-
-  # Misc.
-  htop
-  
-  bat     # replaces cat
-  exa     # replaces ls, tree
-  fasd    # replaces cd
-
-  ripgrep # replaces grep, ag
-  fd      # replaces find
-
-  skim    # fuzzy finder, alternative to fzf
-
-  hexyl  # shows hex code
-  b16sp   # base16-shell-preview
-
-  cht-sh
-  direnv
-  httpie
-]
+    myneovim
+  ]
--- nix/default.nix
+++ nix/default.nix
@@ -1,8 +1,10 @@
 { sources ? import ./sources.nix }:
 
 let
+  # In this section, we add any custom packages we want to be available for installation with `nix-env -i`.
+
   # Use this when want to reference unstable packages
-  nixpkgs-unstable = import sources.nixpkgs-unstable {};
+  #nixpkgs-unstable = import sources.nixpkgs-unstable {};
 
   # Overlay related to package managemnt
   pkgMgmtOverlay = self: super:
